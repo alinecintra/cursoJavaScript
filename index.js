5e9cc6295ca37404b8cdbfb38cd9722e73df87c7
Aline <alineaparecidacintrasilva@gmail.com>
2020-08-27 21:25:38 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// console.log('olá, mundo!')
+// var x = function(x,y) {
+//     return x * y;
+// }
+// console.log(x(2,5))
+
+// function x(a, b) {
+//     return a * b;
+// }
+// console.log(x(2,5))
+
+// const x = (x, y) => x * y;
+// console.log(x(2,5))
+
+// function myFunction(x, y = 10) {
+//     // y é por default
+//     return x + y;
+// }
+// myFunction(5);
+// console.log(myFunction(2))
+
+// const hello = "hello, world";
+// console.log(hello);
+
+// console.log(hello.replace("hello", "goodbye")); // goodbye, world
+
+// console.log("hello".toUpperCase());
+
+// const pessoa = {
+//     nome: 'Jorge',
+//     idade: 28,
+//     cargo: 'Desenvolvedor'
+//   };
+
+//   console.log(pessoa.nome);
+
+// function checaIdade(age){
+//     if (age < 18) {
+//         return "jovem";
+//     } else {
+//         return "Adulto";
+//     }    
+// }
+// const age = checaIdade(25);
+// console.log(age);
+
+// const age = 5;
+// const pessoa = (age >= 18) ? "Adulto": "Criança";
+// console.log(pessoa);
+
+// function teste(a, b, ...args) {
+//      console.log(a); 
+//      console.log(b);
+//      console.log(args);
+//   }
+// (teste(1,2,3,4));
+
+// const pessoa = {
+//     nome: 'Jorge',
+//     idade: 28,
+//     cargo: 'Desenvolvedor'
+//   };
+
+// // const { nome, idade, cargo } = pessoa;
+// // console.log(nome); // Jorge
+// // console.log(idade); // 28
+// // console.log(cargo); // Desenvolvedor
+
+// const { nome, ...rest } = pessoa;
+// console.log(nome); // Jorge
+// console.log(rest.idade); // 28
+// console.log(rest.cargo); // Desenvolvedor
+
+
+
+// function x(a, b) {
+//     return a * b;
+// }
+// console.log(x(2,5))
