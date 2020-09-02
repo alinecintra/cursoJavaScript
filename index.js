09e33426ce6d1fe7b928d80bb05837b8a28b9ce2
Aline <alineaparecidacintrasilva@gmail.com>
2020-09-01 21:56:47 -0300

    aula de 01-09

--- index.js
+++ index.js
@@ -77,3 +77,98 @@
 //     return a * b;
 // }
 // console.log(x(2,5))
+
+//criar uma constante que vai se chamar cor, mvai ter como valor AZUL
+//criar um objeto chamado carro, vai ter como modelo FUSCA, marcar VOLKS
+//E Portas
+
+// CONSTRUCT - inclui atributos de um outro objeto
+// const carroVelho = {
+//     cor: 'vermelho',
+//     status: 'quebrado'
+// } 
+// const cor = 'azul';
+// const carro = {
+//     modelo: 'fusca',
+//     marca: 'volkswagen',
+//     portas: 2,
+//     ...carroVelho
+// };
+// console.log(carro);
+
+
+//criar uma função 
+// banana
+// maça
+// pera
+//uva
+
+// function escolheFruta(fruta) {
+//     // if (fruta === 1) {
+//     //     return 'banana';
+//     // }
+//     // if (fruta === 2) {
+//     //     return 'maçã';
+//     // }
+//     // if (fruta === 3) {
+//     //     return 'pêra';
+//     // }
+//     // if (fruta === 4) {
+//     //     return 'uva'
+//     // }
+//     // throw Error('fruta não encontrada');
+//     switch (fruta) {
+//         case 1:
+//             return 'banana';
+//         case 2:
+//             return 'maça';
+//         case 3:
+//             return 'pêra';
+//         case 4:
+//             return 'uva';
+//         default:
+//             throw Error('Fruta não encontrada');
+//     }
+// }
+
+// function chamaFruta() {
+
+//     try {
+//         const fruta = escolheFruta(5);
+//         console.log(fruta);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
+// chamaFruta()
+
+(() => {
+    console.log('exemplo de callback');
+
+    getLista(10, (err, result) => {
+        if (err) {
+            console.error('excedeu a lista');
+            return;
+        }
+
+        console.log(result);
+    })
+
+})();
+
+function getLista(qtd, callback) {
+    const lista = [];
+
+    for (let i = 0; i < qtd; i++) {
+        lista.push(i);
+    }
+
+    if (lista.length > 5) {
+        return callback(new Error('limite excedido de lista'), null);
+    }
+
+    return callback(null, lista);
+}
+
+
+
